b51cb946503e56734d461231a6524be64fa6879a
Chong <gaochongo@outlook.com>
2026-06-06 20:21:56 +0800

    Adjust quiz panel flex layout and bump version
    Improve quiz panel layout and scrolling: make .quiz-panel-content and .quiz-home use flex column flow, set min-height: 0, add display:flex and overflow rules, prevent header/mode elements from flexing, and remove the max-height on .quiz-home-list to avoid clipping. Also update plugin version to 0.13.4.

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-quiz
 # about: Quiz panel with question bank for Discourse.
-# version: 0.13.3
+# version: 0.13.4
 # authors: howhy.day
 # url: https://github.com/howhy-day/discourse-quiz
 
